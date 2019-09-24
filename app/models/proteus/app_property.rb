bf3ac46392ea3dca5cdc774de68c23f36eed3e45
Colin Petruno <colinpetruno@gmail.com>
2019-09-24 16:10:41 +0200

    Move encryption up to base class

--- app/models/proteus/app_property.rb
+++ app/models/proteus/app_property.rb
@@ -2,11 +2,6 @@ module Proteus
   class AppProperty < Property
     belongs_to :whitelabeled_domain, foreign_key: :proteus_white_labeled_domain_id
 
-    if Proteus.configuration.encryption_engine&.to_s&.downcase == "portunus"
-      include Encryptable
-      encrypted_fields :value
-    end
-
     def self.model_name
       Proteus::Property.model_name
     end
--- app/models/proteus/property.rb
+++ app/models/proteus/property.rb
@@ -1,5 +1,10 @@
 module Proteus
   class Property < ActiveRecord::Base
+    if Proteus.configuration.encryption_engine&.to_s&.downcase == "portunus"
+      include Encryptable
+      encrypted_fields :value
+    end
+
     belongs_to(
       :whitelabeled_domain,
       foreign_key: :proteus_white_labeled_domain_id,
